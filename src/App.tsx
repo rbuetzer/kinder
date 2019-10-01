27d968e868cd4ec344f36564766a4f01147b5269
Richard Bützer <richard@fam-buetzer.ch>
2019-10-02 00:05:50 +0200

    Import names on startup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import * as React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { actions } from "./redux/store";
+
+import names from "./data/names.json";
 
 export const App: React.FunctionComponent = () => {
+  const dispatch = useDispatch();
+
+  const importNames = () => {
+    dispatch(actions.importNames(names));
+  };
+
+  useEffect(importNames, []);
+
   return <>Hello, World </>;
 };
