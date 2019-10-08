ce96cc38a8d95d99fc43f6648e77438004804ee8
Richard Bützer <richard@fam-buetzer.ch>
2019-10-08 15:28:42 +0200

    Extract FileDialog to own component

---
+++ src/nameList/FileDialog.tsx
@@ -0,0 +1,33 @@
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { sagaActions } from "../redux/sagas";
+
+export const FileDialog: React.FunctionComponent = () => {
+  const fileUpload = useRef<HTMLInputElement>();
+  const dispatch = useDispatch();
+
+  const importFile = async (e: any) => {
+    const fileToLoad = (fileUpload.current as any).files[0];
+
+    var fileReader = new FileReader();
+
+    fileReader.onload = function(fileLoadedEvent) {
+      const text = fileLoadedEvent.target.result as string;
+      dispatch(sagaActions.importFile(text));
+    };
+
+    fileReader.readAsText(fileToLoad, "UTF-8");
+  };
+
+  return (
+    <form className={"p-5"}>
+      <input ref={fileUpload} type={"file"} onInput={importFile} hidden />
+      <a
+        onClick={() => fileUpload.current.click()}
+        className={"btn btn-primary text-lg"}
+      >
+        Datei importieren
+      </a>
+    </form>
+  );
+};
--- src/nameList/NameList.tsx
+++ src/nameList/NameList.tsx
@@ -1,36 +1,11 @@
-import React, { useRef, useState } from "react";
-import { file } from "@babel/types";
-import { useDispatch } from "react-redux";
-import { sagaActions } from "../redux/sagas";
+import React from "react";
+import { FileDialog } from "./FileDialog";
+import { Presets } from "./Presets";
 
 export const NameList: React.FunctionComponent = () => {
-  const fileUpload = useRef<HTMLInputElement>();
-  const dispatch = useDispatch();
-
-  const importFile = async (e: any) => {
-    const fileToLoad = (fileUpload.current as any).files[0];
-
-    var fileReader = new FileReader();
-
-    fileReader.onload = function(fileLoadedEvent) {
-      const text = fileLoadedEvent.target.result as string;
-      dispatch(sagaActions.importFile(text));
-    };
-
-    fileReader.readAsText(fileToLoad, "UTF-8");
-  };
-
   return (
     <>
-      <form className={"p-5"}>
-        <input ref={fileUpload} type={"file"} onInput={importFile} hidden />
-        <a
-          onClick={() => fileUpload.current.click()}
-          className={"btn btn-primary text-lg"}
-        >
-          Datei importieren
-        </a>
-      </form>
+      <FileDialog />
     </>
   );
 };
