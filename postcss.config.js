9ffb2d4097bba0f5ce4f46fe3b01d537eb3912e4
Richard Bützer <richard@fam-buetzer.ch>
2019-10-13 11:15:39 +0200

    Add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   modules: true,
-  plugins: [require("tailwindcss"), require("postcss-nested")]
+  plugins: [
+    require("tailwindcss"),
+    require("postcss-nested"),
+    ...(process.env.NODE_ENV === "production"
+      ? [require("@fullhuman/postcss-purgecss")]
+      : [])
+  ]
 };
